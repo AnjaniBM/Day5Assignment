9edc8c4f7e9db7d7642fda4e865565221a0a4278
Anjani B M <anjani.b-m@dxc.com>
2024-02-19 07:34:02 +0530

    Add project files.

---
+++ Day5Assignment/LoginPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Day5Assignment
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if(uName.Text == "admin" && pass.Text == "admin")
+            { 
+              Navigation.PushAsync(new WelcomePage());
+            }
+            else
+            {
+                 DisplayAlert("Login Failed", "Please try again", "OK");
+            }
+
+        }
+    }
+}
